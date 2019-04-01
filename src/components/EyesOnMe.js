cfde833961481733b24554ab9df6834657522ca9
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-01 16:18:47 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+  render(){
+
+    let onFocus = () => {console.log('Good!')}
+
+    let onBlur = () => {console.log('Hey! Eyes on me!')}
+
+
+    return <button onFocus={onFocus} onBlur={onBlur}></button>
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+  render(){
+
+    let onKeyUp = () => {console.log('Entering password...')}
+
+    return <input type="password" onKeyUp={onKeyUp}></input>
+  }
+}
