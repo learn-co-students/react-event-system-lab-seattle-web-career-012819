aa3ed2c48aaf6074d0506310976ff66d5dd00c9d
Nat-Reid <nathanielreid@gmail.com>
2019-04-02 13:16:00 -0700

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component{
+  handleKeyUp = (ev) => {console.log("Entering password...")}
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.handleKeyUp} />
+    )
+  }
+}
