499a198049424f7ea3e41ce5623478626d17305d
allen-woods <allen.woods@flatironschool.com>
2019-04-02 17:12:26 -0700

    First commit.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+  focusHandler = (event) => {
+    console.log('Good!')
+  }
+  blurHandler = (event) => {
+    console.log('Hey! Eyes on me!')
+  }
+  render() {
+    return (
+      <div>
+        <button onFocus={this.focusHandler} onBlur={this.blurHandler}>
+          Lookit Me
+        </button>
+      </div>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+class KeyPad extends Component {
+  keyUpHandler = (event) => {
+    console.log('Entering password...')
+  }
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.keyUpHandler} />
+      </div>
+    )
+  }
+}
+export default KeyPad
